381ea6dd33f10f8084aba9195b7ff9f9ef609216
vishvanatarajan <vishvanatarajan@gmail.com>
2018-01-25 19:19:32 +0530

    Create initial app structure

--- app/src/main/java/com/droidbots/phonemate/SplashActivity.java
+++ app/src/main/java/com/droidbots/phonemate/SplashActivity.java
@@ -1,7 +1,8 @@
 package com.droidbots.phonemate;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(intent);
     }
 }
